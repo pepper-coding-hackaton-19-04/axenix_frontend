e25cbc2759c37197a9e940238b4f563a30a62364
Timur <timengalytchev@mail.ru>
2024-04-21 01:24:39 +0300

    сотрудники

--- src/entities/User/model/types/UserSchema.tsx
+++ src/entities/User/model/types/UserSchema.tsx
@@ -2,4 +2,11 @@ export interface UserSchema {
     userData: null | User
 }
 
-export interface User {}
+export interface User {
+    id: string;
+    role: string;
+    login: string;
+    password: string;
+    created_at: Date;
+    updated_at: Date;
+}
